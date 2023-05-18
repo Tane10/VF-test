f4b21c20441d2faec0480fe6b03d6393866d51d2
JoshThom-itv <tane2@hotmail.co.uk>
2023-05-18 15:25:47 +0100

    submit working + callback + stat updats

--- src/App.js
+++ src/App.js
@@ -1,51 +1,87 @@
-import logo from './logo.svg';
 import './App.css';
-import React, { useState } from 'react';
+import React, { useState, useCallback } from 'react';
 
-// fetch county and return JSON data
-// {statusCode, data: Array<Records>}
-// useCallback to populate Card comp
-// formate response to only name object
-// make it type safe
 const fetchCountyByName = async (name) => {
-  const baseUrl = 'https://restcountries.com/v3.1/name/';
+  let result = {
+    statusCode: 200,
+    data: [],
+    message: '',
+  };
 
-  // await fetch(baseUrl, {});
+  const response = await fetch(`https://restcountries.com/v3.1/name/${name}`, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
 
-  // return {
-  //   statusCode,
-  //   data
-  // }
+  const data = await response.json();
+
+  if (response.status !== 200) {
+    result.statusCode = response.status;
+    result.message = data.message;
+  } else {
+    result.data = data;
+  }
+
+  return result;
 };
 
-const handleFormSubmit = () => {};
-
-// Form
-// validation (onOnly Strings) => disable + enable button
 // handle Errors
-// handle submit
-// clear input
-// handle state change
-// limit: number
-const SearchByNameForm = () => {
+// Limit
+const SearchByNameForm = ({
+  handleFormSubmitCallBack,
+  handleFormResetCallBack,
+}) => {
+  const [countryName, setCountryName] = useState('');
+
+  const handleFormSubmit = async (e) => {
+    e.preventDefault();
+    const countryData = await fetchCountyByName(countryName);
+    handleFormSubmitCallBack(countryData);
+  };
+
+  const handleFormReset = async (e) => {
+    e.preventDefault();
+    setCountryName('');
+    handleFormResetCallBack();
+  };
+
+  const handleOnChangeInput = (value) => {
+    // only allow string
+    if (!/[0-9]/.test(value)) {
+      setCountryName(value);
+    }
+  };
+
   return (
     <div className="SearchByNameForm">
-      <form onSubmit={handleFormSubmit}>
+      <form onSubmit={handleFormSubmit} onReset={handleFormReset}>
         <input
           className="SearchByNameForm-input"
           type="text"
           placeholder="Country name"
+          required
+          value={countryName}
+          onChange={(e) => handleOnChangeInput(e.target.value)}
         />
-        <button className="SearchByNameForm-button" type="submit">
+        <span className="Input error"></span>
+
+        <button
+          className="SearchByNameForm-primary"
+          type="submit"
+          disabled={!countryName}
+        >
           Submit
+        </button>
+        <button className="SearchByNameForm-secondary" type="reset">
+          reset
         </button>
       </form>
     </div>
   );
 };
 
-// Card
-// response state
 const Card = ({ children }) => {
   return (
     <div className="Card">
@@ -54,64 +90,62 @@ const Card = ({ children }) => {
   );
 };
 
-// hold
-const CardContent = ({ common, official }) => {
+const CardContent = ({ props }) => {
+  const {
+    name: { common, official, nativeName: nativeNameObject },
+  } = props;
+
+  const key = Object.keys(nativeNameObject)[0];
+  const nativeName = nativeNameObject[key]['official'];
+
   return (
     <div>
-      <span>Common Name: {common}</span>
-      <span>Official Name: {official}</span>
+      <div className="CardContent-div">
+        <span>Common Name:</span>
+        <span className="CardContent-text"> {common}</span>
+      </div>
+      <div className="CardContent-div">
+        <span>Official Name:</span>
+        <span className="CardContent-text">{official}</span>
+      </div>
+      <div className="CardContent-div">
+        <span>Native Name:</span>
+        <span className="CardContent-text">{nativeName}</span>
+      </div>
     </div>
   );
 };
 
-// return response on a array of card in
-/**
- * <Container>
- *  res.map((r) => {
- *  return (<>
- * <Card props/> )})
- */
-
 const App = () => {
-  const [countries, setCountries] = useState([
-    {
-      common: 'Spain',
-      official: 'Kingdom of Spain',
-      nativeName: {
-        spa: { official: 'Reino de España', common: 'España' },
-      },
-    },
-    {
-      common: 'Spain',
-      official: 'Kingdom of Spain',
-      nativeName: {
-        spa: { official: 'Reino de España', common: 'España' },
-      },
-    },
-    {
-      common: 'Spain',
-      official: 'Kingdom of Spain',
-      nativeName: {
-        spa: { official: 'Reino de España', common: 'España' },
-      },
-    },
-  ]);
+  const [countries, setCountries] = useState([]);
+
+  const onHandleFormSubmitCallBack = useCallback((fetchedCountries) => {
+    setCountries(fetchedCountries.data);
+  }, []);
+
+  const onHandleFormResetCallBack = useCallback(() => {
+    setCountries([]);
+  }, []);
 
   return (
     <div className="Container">
       <Card>
         <span className="Title"> Search by county name</span>
-        <SearchByNameForm />
+        <SearchByNameForm
+          handleFormSubmitCallBack={onHandleFormSubmitCallBack}
+          handleFormResetCallBack={onHandleFormResetCallBack}
+        />
         <div className="Content">
-          {countries.map((country) => {
-            return (
-              <>
-                <Card>
-                  <CardContent details={country} />
-                </Card>
-              </>
-            );
-          })}
+          {countries &&
+            countries.map((country) => {
+              return (
+                <>
+                  <Card>
+                    <CardContent props={country} />
+                  </Card>
+                </>
+              );
+            })}
         </div>
       </Card>
     </div>
